f29234412f8921bc64610fefdaf1fd1ec74fd246
Karo <bananusek@tlen.pl>
2015-08-16 19:42:06 +0200

    hmm

--- proj2/src/proj2/TapeEquilibrium.java
+++ proj2/src/proj2/TapeEquilibrium.java
@@ -6,11 +6,27 @@ class TapeEquilibrium {
     		
     		TapeEquilibrium te = new TapeEquilibrium();
     		int najmniejsza = te.solution(new int[]{3,1,2,4,3});
-    		System.out.println("najmniejsza" -> " + najmniejsza);
+    		System.out.println("najmniejsza -> " + najmniejsza);
     }
     	
     	public int solution(int[] A)
     	{
+    		int L;
+    		int P;
+    		int suma;
+    		
+    		for (int i = 0; i < A.length; i++) {
+        		
+    		suma = suma + A[i];
+    		}
+   
+        	while (i < A.length+1) {
+        		
+        		L=L+A[i];
+        		P=suma-L;	
+        				
+        		}
+        	return 0;
     		
     		
     		
