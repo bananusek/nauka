73dcbb2acd6b86a11563eae4ba3982e7b5e26ecc
Karo <bananusek@tlen.pl>
2015-08-16 18:45:01 +0200

    2

---
+++ proj2/src/proj2/TapeEquilibrium.java
@@ -0,0 +1,22 @@
+
+
+class TapeEquilibrium {
+    
+    	public static void main(String[] args) {
+    		
+    		TapeEquilibrium te = new TapeEquilibrium();
+    		int najmniejsza = te.solution(new int[]{3,1,2,4,3});
+    		System.out.println("brakujaca -> " + najmniejsza);
+    }
+    	
+    	public int solution(int[] A)
+    	{
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    	}
+}
