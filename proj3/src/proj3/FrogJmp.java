3e3f85f30a679113bbcea34b3916bf505ad38870
Karo <bananusek@tlen.pl>
2015-08-23 21:13:16 +0200

    siemka Stawik :D

--- proj3/src/proj3/FrogJmp.java
+++ proj3/src/proj3/FrogJmp.java
@@ -6,7 +6,7 @@ public class FrogJmp
 	public static void main(String[] args){
 		
 		FrogJmp fj = new FrogJmp();
-		int skok = fj.solution (new int 10 ,85, 30);
+		int skok = fj.solution (new int 10 ,85, 30);//tu chyba jest coœ nie tak przeciesz 10 i 85 to inty?
 		System.out.println("skoczy" + skok + "razy");
 		
 		
@@ -15,25 +15,22 @@ public class FrogJmp
 		
 		class Solution {
 		    public int solution(int X, int Y, int D) {
-		        // write your code in Java SE 8
+		        
 		    	
 		    	int jump;
+		    	int reszta;
 		    	
-		    	jump=(Y-X)/D;
+		    	jump=(Y-X)/ D;
+		    	reszta=(Y-X)% D;
+		    	if (reszta != 0) {
 		    	
 		    	jump=jump+1;
 		    	
+		    	}
 		    	
 		    	return jump;
 		    	
-		
-		
-		
-		
-		
-		
-		    	return 0;	
-			}
+		    }
 
 }
 }
