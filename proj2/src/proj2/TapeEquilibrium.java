7643895af8b2d8218594f4b95ac89c267856fe22
Karo <bananusek@tlen.pl>
2015-08-23 21:41:51 +0200

    ?

--- proj2/src/proj2/TapeEquilibrium.java
+++ proj2/src/proj2/TapeEquilibrium.java
@@ -5,38 +5,41 @@ class TapeEquilibrium {
     	public static void main(String[] args) {
     		
     		TapeEquilibrium te = new TapeEquilibrium();
-    		int najmniejsza = te.solution(new int[]{3,1,2,4,3});
+    		int najmniejsza = te.solution (new int[]{3,1,2,4,3});
     		System.out.println("najmniejsza -> " + najmniejsza);
     }
     	
-    	public int solution(int[] A)
-    	{
-    		int L;
-    		int P;
-    		int suma;
-    		int roznica;
-    		int poprzednia;
- 
-    		for (int i = 0; i < A.length; i++) {
-        		
-    		suma = suma + A[i];
-    		}
-    		
-    		roznica=0;
-    		int i;		
-        	while  (i < A.length+1) {
-        		roznica=poprzednia;
-        		L=L+A[i];
-        		P=suma-L;
-        		roznica=P=L;
-        		if(roznica<poprzednia){
-        			poprzednia=roznica;
-        			return roznica;
-        		}
 
-    		
-    		return 0;
-    		
-    		
+    	class Solution {
+    	    public int solution(int[] A) {
+    	        // write your code in Java SE 8
+    	        int L =0;
+    	    		int P;
+    	    		int suma =0 ;
+    	    		int roznica =0;
+    	    		int poprzednia=Integer.MAX_VALUE;
+    	 
+    	    		for (int i = 0; i < A.length; i++) {
+    	        		
+    	    		suma = suma + A[i];
+    	    		}
+    	    		
+    	    		int i=0;		
+    	        	while  (i < A.length) {
+    	        		roznica=poprzednia;
+    	        		L=L+A[i];
+    	        		P=suma-L;
+    	        		roznica=Math.abs(P-L);
+    	        		if(roznica<poprzednia){
+    	        			poprzednia=roznica;
+    	        		}
+    	        		i++;
+    	        	}
+    	        			return poprzednia;
+    	        
+    	        	
+    	    		
+    	    	
+    	    }
     	}
 }
