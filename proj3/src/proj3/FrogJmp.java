e49556753e1da4c60e926f050b82150cdc939dbf
Karo <bananusek@tlen.pl>
2015-08-25 07:38:22 +0200

    juhuuuu działa

--- proj3/src/proj3/FrogJmp.java
+++ proj3/src/proj3/FrogJmp.java
@@ -6,14 +6,14 @@ public class FrogJmp
 	public static void main(String[] args){
 		
 		FrogJmp fj = new FrogJmp();
-		int skok = fj.solution (new int 10 ,85, 30);//tu chyba jest coœ nie tak przeciesz 10 i 85 to inty?
+		int skok = fj.solution(10 ,85, 30);//tu chyba jest coœ nie tak przeciesz 10 i 85 to inty?
 		System.out.println("skoczy" + skok + "razy");
 		
 		
 		}
 		
 		
-		class Solution {
+	
 		    public int solution(int X, int Y, int D) {
 		        
 		    	
@@ -32,5 +32,4 @@ public class FrogJmp
 		    	
 		    }
 
-}
 }
