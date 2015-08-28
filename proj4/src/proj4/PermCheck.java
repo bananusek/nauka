60f7e67278f1b8a9b5d21e9c8cf43d4b91a3b6b9
Karo <bananusek@tlen.pl>
2015-08-28 13:52:48 +0200

    ju huhuhuhu

--- proj4/src/proj4/PermCheck.java
+++ proj4/src/proj4/PermCheck.java
@@ -7,7 +7,7 @@ public class PermCheck {
 	public static void main(String[] args) {
 	
 		PermCheck pme = new PermCheck();
-		int perm = pme.solution(new int[]{4,1,3,2});// powinno wyjsc 0
+		int perm = pme.solution(new int[]{4,3,2});// powinno wyjsc 0
 		System.out.println("czy jest? " + perm);
 	
 	
@@ -20,20 +20,18 @@ public class PermCheck {
 	    
 	    java.util.Arrays.sort(A);
 	    
-	    
-	    
+	    if(A[0]!=1){
+	    return 0; }
 	    
 	    for( int i=0; i< A.length -1 ; i++){
 	        // tutaj w ogole przy i++ wyswietla sie komunikat ze jest to dead code :(? 
 	        
 	        if (A[i+1]!= A[i] +1){
 	        
-	         //// tutaj nic
+	         return 0;
 	        }
 	        
-	        ///jesli wszystkie nie
-	        
-	        return 0;     
+	            
 	    }
 	    
 	     return 1;
