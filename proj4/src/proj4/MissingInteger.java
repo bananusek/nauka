fce91e7953711e8345b6153ae6c0567dccbdaa38
Karo <bananusek@tlen.pl>
2015-08-31 23:29:48 +0200

    ehh

--- proj4/src/proj4/MissingInteger.java
+++ proj4/src/proj4/MissingInteger.java
@@ -2,49 +2,37 @@ package proj4;
 
 public class MissingInteger {
 
-	
-	
 	public static void main(String[] args) {
 
 		MissingInteger pme = new MissingInteger();
-		int perm = pme.solution(new int[] { 1,3,6,4,1,2 });
+		int perm = pme.solution(new int[] { 1, 3, 6, 4, 1, 2 });
 		System.out.println("ktora-->" + perm);
-	
+
 	}
-	
-	
-	
-	
-	
-	
+
 	int solution(int[] A) {
-	    java.util.Arrays.sort(A);  // {1,1,2,3,4,6}
-	    	
-	    int brakujaca=0;	
-			for (int i = 0; i < A.length - 1;) { // 2==1+1 to i=3, 3==2+1 to i=4, 4==3+1 to i==5,
-			
-	        
-	            if (A[i + 1] == A[i] + 1) { 
+		java.util.Arrays.sort(A); // {1,1,2,3,4,6}
 
-	             i++;
+		int brakujaca = 0;
+		for (int i = 0; i < A.length - 1;) { // 2==1+1 to i=3, 3==2+1 to i=4,
+												// 4==3+1 to i==5,
 
-	                } else if (A[i + 1] == A[i]) { // 1. 1==1  to i=2,
-	    
-	                     i++;
-	                } else {    //6!==4+1  wtedy
-	                	
-	                	 brakujaca=A[i]; // 4+1=5
-	                	
-	                	break; //5
-	                }
-	        
-	        
-	        
-	        
-			
+			if (A[i + 1] == A[i] + 1) {
+
+				i++;
+
+			} else if (A[i + 1] == A[i]) { // 1. 1==1 to i=2,
+
+				i++;
+			} else { // 6!=4+1 wtedy
+
+				brakujaca = A[i - 1] + 1; // 4+1=5
+
+				break; // 5
+			}
 
 		}
-			return brakujaca; /// wychodzi 4 a nie pieæ
+		return brakujaca; /// wychodzi 4 a nie pieæ
 
-}
+	}
 }
