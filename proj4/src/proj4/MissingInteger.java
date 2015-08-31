091bef510363ac0c30349e520a4de7af8a79c369
Karo <bananusek@tlen.pl>
2015-08-31 23:17:26 +0200

    lool

--- proj4/src/proj4/MissingInteger.java
+++ proj4/src/proj4/MissingInteger.java
@@ -18,30 +18,32 @@ public class MissingInteger {
 	
 	
 	int solution(int[] A) {
-	    java.util.Arrays.sort(A);
-
-			for (int i = 0; i < A.length - 1;) {
+	    java.util.Arrays.sort(A);  // {1,1,2,3,4,6}
+	 
+			for (int i = 0; i < A.length - 1;) { // 2==1+1 to i=3, 3==2+1 to i=4, 4==3+1 to i==5,
 			
 	        
-	            if (A[i + 1] == A[i] + 1) {
+	            if (A[i + 1] == A[i] + 1) { 
 
 	             i++;
 
-	                } else if (A[i + 1] == A[i]) {
+	                } else if (A[i + 1] == A[i]) { //1==1  to i=2,
 	    
 	                     i++;
-	                } else {
-
-	                	return A[i];
+	                } else {    //6!==4+1  wtedy
+	                	
+	                	int brakujaca=A[i-1]+1; // 4+1=5
+	                	
+	                	return brakujaca; //5
 	                }
 	        
 	        
 	        
 	        
-			return A[i];
+			return 0;
 
 		}
-			return -1;
+			return 0; /// wychodzi 0
 
 }
 }
