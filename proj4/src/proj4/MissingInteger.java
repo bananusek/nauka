8a0057cc1f71889e6572cee2329797be9e8a5fdc
Karo <bananusek@tlen.pl>
2015-09-01 09:33:22 +0200

    missinginteger

--- proj4/src/proj4/MissingInteger.java
+++ proj4/src/proj4/MissingInteger.java
@@ -5,7 +5,7 @@ public class MissingInteger {
 	public static void main(String[] args) {
 
 		MissingInteger pme = new MissingInteger();
-		int perm = pme.solution(new int[] { 1, 3, 6, 4, 1, 2 });
+		int perm = pme.solution(new int[] { -1, -3,-6, -4, -1, -2 });
 		System.out.println("ktora-->" + perm);
 
 	}
@@ -16,7 +16,7 @@ public class MissingInteger {
 		int brakujaca = 0;
 		for (int i = 0; i < A.length - 1;) { // 2==1+1 to i=3, 3==2+1 to i=4,
 												// 4==3+1 to i==5,
-
+			System.out.println(A[i]);
 			if (A[i + 1] == A[i] + 1) {
 
 				i++;
@@ -26,13 +26,13 @@ public class MissingInteger {
 				i++;
 			} else { // 6!=4+1 wtedy
 
-				brakujaca = A[i - 1] + 1; // 4+1=5
-
-				break; // 5
+				brakujaca = A[i] + 1;
+				
+				break;//; //
 			}
 
 		}
-		return brakujaca; /// wychodzi 4 a nie pieæ
+		return brakujaca; //
 
 	}
 }
