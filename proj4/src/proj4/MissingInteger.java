c165bb654c6f73d00ef97cc2a3f0fa0e42c321ed
Karo <bananusek@tlen.pl>
2015-09-01 10:34:09 +0200

    #1 missinginteger

--- proj4/src/proj4/MissingInteger.java
+++ proj4/src/proj4/MissingInteger.java
@@ -5,37 +5,38 @@ public class MissingInteger {
 	public static void main(String[] args) {
 
 		MissingInteger pme = new MissingInteger();
-		int perm = pme.solution(new int[] { -6,-5,-3 });
+		int perm = pme.solution(new int[] { -5, -3 });
 		System.out.println(perm);
 
 	}
 
 	int solution(int[] A) {
-		java.util.Arrays.sort(A); // {1,1,2,3,4,6}
+		java.util.Arrays.sort(A);
 
 		int brakujaca = 0;
-		for (int i = 0; i < A.length - 1;) { 
+		for (int i = 0; i < A.length - 1;) {
 			System.out.println(A[i]);
 			if (A[i + 1] == A[i] + 1) {
 
 				i++;
 
-			} else if (A[i + 1] == A[i]) { 
+			} else if (A[i + 1] == A[i]) {
 
 				i++;
-			} else { 
+			} else {
 
 				brakujaca = A[i] + 1;
 				System.out.println("brakujaca to:");
-				break;// ; //
+				return brakujaca;
+
 			}
 
 		}
-		if (brakujaca == 0) {
 
-			System.out.println("nie ma brakujacej");
-			
-			return 0;
+		if (brakujaca  <= 0) {
+
+			brakujaca = 1;
+
 		}
 		return brakujaca;
 	}
