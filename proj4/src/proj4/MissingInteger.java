03e665ca5ad978f79a1d7e194956b9a955828027
Karo <bananusek@tlen.pl>
2015-09-02 22:17:00 +0200

    #missinginteger

--- proj4/src/proj4/MissingInteger.java
+++ proj4/src/proj4/MissingInteger.java
@@ -5,48 +5,53 @@ public class MissingInteger {
 	public static void main(String[] args) {
 
 		MissingInteger pme = new MissingInteger();
-		int perm = pme.solution(new int[] {-5,-4,-3,-2});
+		int perm = pme.solution(new int[] {-1});
 		System.out.println(perm);
 
 	}
 
+	public int solution(int[] A) {
+		java.util.Arrays.sort(A);
 
-	    public int solution(int[] A) {
-	      java.util.Arrays.sort(A);
-	
-			int brakujaca = 0;
-			for (int i = 0; i < A.length - 1;) {
+		int brakujaca = 0;
+		
+		int i = 0; 
+		
+		while(A[i]<0) i ++;{
+		
+		for ( ;i < A.length -1;) {
 			
-				if (A[i + 1] == A[i] + 1) {
-					
-					i++;
+			if (A[i + 1] == A[i] + 1) {
 
-				} else if (A[i + 1] == A[i]) {
-					
-					i++;
-				} else {
+				i++;
 
-					brakujaca = A[i] + 1;
-				
-					
+			} else if (A[i + 1] == A[i]) {
 
-				}
+				i++;
+
+			} else {
+
+				brakujaca = A[i] + 1;
+
+				break;
 
 			}
+		}/// jak usuwam tego whila to jest okej i zwraca dla -1  jedynkê
+		
+		System.out.println(brakujaca);
+		if (brakujaca == 0) {
 
-			if (brakujaca < 0) {
+			brakujaca = A[A.length - 1] + 1;
 
-				brakujaca = 1;
-
-			}
-			
-			else if(brakujaca==0){
-			
-				brakujaca=A[A.length-1]+1;
-			}
-			
-			return brakujaca;
 		}
+		if (brakujaca <= 0) {
 
+			brakujaca = 1;
 
+		}
+	
+
+		return brakujaca;
 	}
+	}
+}
