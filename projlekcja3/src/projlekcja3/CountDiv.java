bbde8a56b3527304a503e29d19b68212e17a0519
Karo <bananusek@tlen.pl>
2015-09-04 16:45:42 +0200

    #3CountDiv

--- projlekcja3/src/projlekcja3/CountDiv.java
+++ projlekcja3/src/projlekcja3/CountDiv.java
@@ -4,25 +4,28 @@ public class CountDiv {
 
 	public static void main(String[] args) {
 		CountDiv dv = new CountDiv();
-		int div = dv.solution(6);
-		System.out.println("w przedziale od A do B  " + div + " jest podzielnych przez K");
+		int div = dv.solution(0,1, 11);
+		System.out.println( div );
 	}
 
-	public int solution(int B) {
+	public int solution(int A, int B, int K) {
 		int ile = 0;
-		int [] tab = new int[B];
-		for(int i=0; i<B ;i++){
 		
-		tab[i]=i+1;                    						/// to powinno nadac od 1,2,3,4,5,6,7,8,9,10,11
+		
+		int[] tab = new int[B];
+		for (int i = A; i <=B; i++) {
+
+			tab[i] = i ; /// to powinno nadac od 1,2,3,4,5,6,7,8,9,10,11
+			System.out.println("tablica to " + tab[i]+ " dla i = "+i);
 		}
-		
-		System.out.println("tablica to"+ tab);
-		
-		
-		
-		
-		
-		
+
+		for (int j = A ; j <=B; j++) {
+			if (tab[j] % K == 0) {
+				ile = ile + 1;
+				System.out.println("podzielna to   " + tab[j] );
+			}
+
+		}
 		return ile;
 
 	}
