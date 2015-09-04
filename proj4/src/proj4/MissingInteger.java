be1dfc27907302b63f29d816e0ad4ccd54bfc3b9
Karo <bananusek@tlen.pl>
2015-09-04 09:41:02 +0200

    #1 missing integer

--- proj4/src/proj4/MissingInteger.java
+++ proj4/src/proj4/MissingInteger.java
@@ -5,7 +5,7 @@ public class MissingInteger {
 	public static void main(String[] args) {
 
 		MissingInteger pme = new MissingInteger();
-		int perm = pme.solution(new int[] {-1});
+		int perm = pme.solution(new int[] { 1, 2, 3, 4, 5, 5, 767, 56, 345 });
 		System.out.println(perm);
 
 	}
@@ -14,13 +14,25 @@ public class MissingInteger {
 		java.util.Arrays.sort(A);
 
 		int brakujaca = 0;
-		
-		int i = 0; 
-		
-		while(A[i]<0) i ++;{
-		
-		for ( ;i < A.length -1;) {
-			
+
+		int niezerowa = 0;
+
+		for (int j = 0; j < A.length - 1; j++) {
+			if (A[j] > 0) {
+
+				niezerowa = j;
+				break;
+			}
+		}
+		if (A[niezerowa] != 1) {
+			return 1;
+		}
+
+		System.out.println("niezerowa  " + niezerowa);
+		int i = niezerowa;
+
+		for (; i < A.length - 1;) {
+
 			if (A[i + 1] == A[i] + 1) {
 
 				i++;
@@ -36,9 +48,8 @@ public class MissingInteger {
 				break;
 
 			}
-		}/// jak usuwam tego whila to jest okej i zwraca dla -1  jedynkê
-		
-		System.out.println(brakujaca);
+		}
+		System.out.println("brakujaca " + brakujaca);
 		if (brakujaca == 0) {
 
 			brakujaca = A[A.length - 1] + 1;
@@ -49,9 +60,8 @@ public class MissingInteger {
 			brakujaca = 1;
 
 		}
-	
 
 		return brakujaca;
-	}
+
 	}
 }
--- proj4/src/proj4/PermCheck.java
+++ proj4/src/proj4/PermCheck.java
@@ -5,39 +5,29 @@ package proj4;
 public class PermCheck {
 
 	public static void main(String[] args) {
-	
+
 		PermCheck pme = new PermCheck();
-		int perm = pme.solution(new int[]{4,3,2});// powinno wyjsc 0
+		int perm = pme.solution(new int[] { 4, 3, 2 });// powinno wyjsc 0
 		System.out.println("czy jest? " + perm);
-	
-	
-	
+
 	}
-	
+
 	int solution(int A[]) {
-	    
-	    
-	    
-	    java.util.Arrays.sort(A);
-	    
-	    if(A[0]!=1){
-	    return 0; }
-	    
-	    for( int i=0; i< A.length -1 ; i++){
-	        // tutaj w ogole przy i++ wyswietla sie komunikat ze jest to dead code :(? 
-	        
-	        if (A[i+1]!= A[i] +1){
-	        
-	         return 0;
-	        }
-	        
-	            
-	    }
-	    
-	     return 1;
-	    
-	    
-	    
-	} 
-	
+
+		java.util.Arrays.sort(A);
+
+		
+		for (int i = 0; i < A.length - 1; i++) {
+			
+			if (A[i + 1] != A[i] + 1) {
+
+				return 0;
+			}
+
+		}
+
+		return j;
+
+	}
+
 }
