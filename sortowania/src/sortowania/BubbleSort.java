94faa63025a3d3a51ec06046638c6f9e65858448
Karo <bananusek@tlen.pl>
2016-02-12 20:44:14 +0100

    #6 poprawka

--- sortowania/src/sortowania/BubbleSort.java
+++ sortowania/src/sortowania/BubbleSort.java
@@ -9,16 +9,14 @@ public class BubbleSort {
 		}
 
 		for (int i = 0; i < A.length - 1; i++) {
-			for (int j = 0; j < A.length - 1; j++) {
 
-				if (A[j] > A[j + 1]) {
-					a = A[j + 1];
-					A[j + 1] = A[j];
-					A[j] = a;
-				}
+			if (A[i] > A[i + 1]) {
+				a = A[i + 1];
+				A[i + 1] = A[i];
+				A[i] = a;
 			}
 		}
-		
+
 		return 1;
 	}
 }
