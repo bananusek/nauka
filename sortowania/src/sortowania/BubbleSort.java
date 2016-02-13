9372d97b9e70f6b18ffc6ae3cbe747a68e3ebf97
Karo <bananusek@tlen.pl>
2016-02-13 10:55:52 +0100

    #6 bubble

--- sortowania/src/sortowania/BubbleSort.java
+++ sortowania/src/sortowania/BubbleSort.java
@@ -3,25 +3,23 @@ package sortowania;
 public class BubbleSort {
 
 	public int solution(int[] A) {
-		int a = 0;
-		if (A.length == 0) {
-			return 0;
-		}
-		for (int i = 0; i < A.length - 1; i++) {
-			for (int j = i; j < A.length - 1; j++) {
-				int swap=0;
-				if (A[j] > A[j + 1]) {
-					a = A[j + 1];
-					A[j + 1] = A[j];
-					A[j] = a;
-					swap = 1;
-				}
-				if (swap==0){
-					return 1;
+		{
+			int i;
+			boolean flaga = true;
+			int temp; 
+			while (flaga) {
+				flaga = false; 
+				for (i = 0; i < A.length - 1; i++) {
+					if (A[i] < A[i + 1]) 
+					{
+						temp = A[i]; 
+						A[i] = A[i + 1];
+						A[i + 1] = temp;
+						flaga = true;
+					}
 				}
 			}
+			return 1;
 		}
-
-		return 1;
 	}
 }
